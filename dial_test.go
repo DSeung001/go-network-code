eb998fc5c4f7ba9460180ce3cb17d1044d5a4c5e
지승렬 <seungryeol156@gmail.com>
2022-11-28 11:24:38 +0900

    3-4 example

--- dial_test.go
+++ dial_test.go
@@ -1,1 +1,67 @@
 package main
+
+import (
+	"io"
+	"net"
+	"testing"
+)
+
+func TestDial(t *testing.T) {
+	// 포트 미 지정시 랜덤 포트로 생성
+	listener, err := net.Listen("tcp", "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	// 1: 핸들러
+	// 클라이언트 수신 연결
+	go func() {
+		defer func() {
+			done <- struct{}{}
+		}()
+
+		for {
+			// 2: 연결해서는 실패가 없이 로그만 출력
+			conn, err := listener.Accept()
+			if err != nil {
+				t.Log(err)
+				return
+			}
+
+			// 3:
+			go func(c net.Conn) {
+				defer func() {
+					c.Close()
+					done <- struct{}{}
+				}()
+
+				buf := make([]byte, 1024)
+				for {
+					// 4: 패킷을 받고 로그, 해당 패킷이 EOF이면 종료
+					n, err := c.Read(buf)
+					if err != nil {
+						if err != io.EOF {
+							t.Error(err)
+						}
+						return
+					}
+					t.Logf("received: %q", buf[:n])
+				}
+			}(conn)
+		}
+	}()
+
+	// 5,6,7
+	// 리스너로 연결
+	conn, err := net.Dial("tcp", listener.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 8
+	conn.Close()
+	<-done
+	listener.Close()
+	<-done
+}
--- listen_test.go
+++ listen_test.go
@@ -7,6 +7,8 @@ import (
 
 func TestListener(t *testing.T) {
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
+
+	// nil은 object c에서 파생된 단어로 객체 참조 전용 null 값으로 Go에서는 null 대신 nil(닐) 사용
 	if err != nil {
 		t.Fatal(err)
 	}
